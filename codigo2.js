ee269a91abaecda0d67a82c64ea63b6e5f6268b0
Alan Gonzalez <alangonzalezmorales2003@gmail.com>
2024-10-29 13:33:15 +0100

    Insertar codigo2

---
+++ codigo2.js
@@ -0,0 +1,17 @@
+// Mensaje de bienvenida
+console.log("¡Hola, bienvenido a primerProyectoJS!");
+
+// Función para sumar dos números
+function sumar(a, b) {
+    return a + b;
+}
+
+// Declaración de variables
+let num1 = 5;
+let num2 = 10;
+
+// Llamada a la función y almacenamiento del resultado
+let resultado = sumar(num1, num2);
+
+// Muestra el resultado en la consola
+console.log("La suma de", num1, "y", num2, "es:", resultado);
